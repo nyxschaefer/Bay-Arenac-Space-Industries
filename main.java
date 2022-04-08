30bc09308f211900a08f4e1864ac45890b192a75
Nyx <gschaefer2004@gmail.com>
2022-04-08 13:12:11 -0400

    n

--- main.java
+++ main.java
@@ -1,7 +1,11 @@
 public class main
 {
-        public static void main(String[] args)
+        public static void Main(String[] args)
         {
-        LoginFormDemo();
+        LoginForm();
         }
+
+        
+
+        
 }

2379b5a6e1ecd1447a33b66fb13baac02b5e4650
MrMustache820 <102991173+MrMustache820@users.noreply.github.com>
2022-04-27 14:08:36 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,7 +11,9 @@ public class Main {
      //   if (passquery = password){
 
         //}
-
+        System.out.println("'Welcome to BASI! You are about to depart on our very first rocket into space!'");
+        System.out.println("'If you look to your left you will see our trusted personel loading the rocket with fuel and suplies.'");
+        System.out.println("You look around the base for a few minutes and decide to enter the rocket without telling anyone or grabing any supplies.");
 int choice;
 do
 {
@@ -38,7 +40,7 @@ do
 
         System.out.println("1. Continue with the mission");
         System.out.println("2. Astronaut information");
-        System.out.println("3. Fuel left in tank");
+        System.out.println("3. Speed and fuel left in tank");
         System.out.println("4. Quit\n");
         System.out.print("Enter your choice (1-4) : ");
 
@@ -54,6 +56,7 @@ return choice;
 }
 
 public static void launch(Scanner keyboard){
+    System.out.println("You stay on the rocket for awhile ");
 System.out.println("\n\nThe rocket launches off of the station at 30 meters per second, within 90 seconds the rocket reaches the speed of 2700 meters \nper second.");
 System.out.println("At this point the engines turn off and within 275 seconds the rockets speed decreases by 2697.75, leaving the speed at 2.25 \nmeters per second.");
 System.out.println("The rocket engines turn back on slowly, incresing the speed by 3.75 meters per second, within 5 seconds the rocket reaches 21 \nmeters per second.");
@@ -63,7 +66,8 @@ public static void database(Scanner keyboard){
     
 }
 public static void triangle(Scanner keyboard){
-    
+    System.out.println("Speed: 2721 meters per second");
+    System.out.println("Fuel left in tank: ");
 }
 
 

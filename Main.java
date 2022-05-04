3f1e840f17da059d9a599bfb98372b9f39037b78
MrMustache820 <102991173+MrMustache820@users.noreply.github.com>
2022-05-04 12:00:51 -0400

    me

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ static void printAndSleep(String string, int sleepTimeMs) throws InterruptedExce
     System.out.println(string);
     Thread.sleep(sleepTimeMs);
   }
-
+//printAndSleep("",2000);
 public static void launch(Scanner keyboard) throws InterruptedException{
     printAndSleep("The rocket slowly prepares to take off in....",2000);
     printAndSleep("3",2000);
@@ -22,10 +22,16 @@ public static void launch(Scanner keyboard) throws InterruptedException{
     printAndSleep("1",2000);
     printAndSleep("\nThe rocket launches off of the station releasing 1 pound of fuel per second, within 60 seconds the rocket reaches the speed of 1800 meters per second.", 3000);
     printAndSleep("By 65 seconds the rocket has reached a speed of 1950 meters per second, and traveled 64,359 meters.",3000);
-    printAndSleep("The rocket leaves the atmosphere by 68 seconds having traveled 70,380 meters.\n\n",3000);
+    printAndSleep("The rocket leaves the atmosphere by 68 seconds going 2040 meters per second and having traveled 70,380 meters.\n\n",3000);
+Menu2.main();
+
 }
+
+
 public static void fuel(Scanner keyboard){
+    
 }
+
 
 //lol
 public static void main() {
---
+++ Menu3.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+
+public class Menu3
+{
+        public static void main() throws InterruptedException
+        {
+                Scanner keyboard = new Scanner(System.in);
+                int choice;
+                String userName = "";
+         do
+         {
+             // Get the user's menu choice.
+             choice = getMenuChoice(keyboard);
+         
+             // Do what the user requested.
+             switch(choice)
+             {
+                 case 1 : menu.main();
+                     break;
+                 case 2 : AstronautInformation.astro(userName);
+                     break;
+                 case 3 : Main.fuel(keyboard);
+                     break;
+                 case 4 : Logout.main();
+             }
+                 } while (choice != 4);
+                     }
+                 
+         
+             public static int getMenuChoice(Scanner keyboard)
+             {
+                 int choice;
+         
+         
+                 System.out.println("1. Start over");
+                 System.out.println("2. Astronaut information");
+                 System.out.println("3. Fuel left in tank");
+                 System.out.println("4. Quit\n");
+                 System.out.print("Enter your choice (1-4) : ");
+                 choice = keyboard.nextInt();
+         
+         if (choice < 1 || choice > 4)
+         {
+             System.out.print("Error: Enter 1, 2, 3, or 4: ");
+             choice = keyboard.nextInt();
+         }
+         
+         return choice;
+         }
+
+         
+
+
+}

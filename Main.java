deeca2f82b85c0cc89edc58c2dad19ef8974db92
Nyx <gschaefer2004@gmail.com>
2022-05-04 14:12:02 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -29,6 +29,7 @@ public static void launch(Scanner keyboard) throws InterruptedException{
     printAndSleep("\nThe rocket launches off of the station releasing 1 pound of fuel per second, within 60 seconds the rocket reaches the speed of 1800 meters per second.", 3000);
     printAndSleep("By 65 seconds the rocket has reached a speed of 1950 meters per second, and traveled 64,359 meters.",3000);
     printAndSleep("The rocket leaves the atmosphere by 68 seconds having traveled 70,380 meters.\n\n",3000);
+    Menu2.main();
 }
 //The amount of fuel in the rocket.
 public static void fuel(Scanner keyboard) throws InterruptedException{
